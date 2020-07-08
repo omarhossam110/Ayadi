592449825755d279886ea24423fa55d4b7cde888
unknown <m.omar.hossam@gmail.com>
2020-07-08 16:26:48 +0200

    new update for ui

--- views/login/js/test.js
+++ views/login/js/test.js
@@ -1,4 +1,5 @@
 var myOpenAye = document.getElementById('open'),
+    myCloseAye = document.getElementById('close'),
     myInput = document.getElementById('op'),
     mySign = document.getElementById('sign'),
     myName = document.getElementById('name'),
@@ -7,69 +8,67 @@ var myOpenAye = document.getElementById('open'),
     myPassword = document.getElementById('password'),
     myMessage = document.getElementById('message'),
     myForm = document.getElementById('main');
-    
 
-    
-    
-
-myOpenAye.onclick = function(){
+myOpenAye.onclick = function () {
     'use strict';
-    if(myInput.type==="password"){
-        myInput.type ="text";
-        
-    }else{
-        myInput.type="password";
-        
-    }
-       
+    // if (myInput.type === "password") {
+    myInput.type = "text";
+    myCloseAye.style.display = "block";
+    myOpenAye.style.display = "none";
+
+    // } else {
+    //     myInput.type = "password";
+    // }
+};
+myCloseAye.onclick = function () {
+    'use strict';
+    myInput.type = "password";
+    myCloseAye.style.display = "none";
+    myOpenAye.style.display = "block";
 };
 
-mySign.onclick = function(){
-    
+mySign.onclick = function () {
     'use strict';
-    
-    if(myName.value === '' || myName.value == null){
-         
-       myMessage.innerHTML="you must write your name";
-        
-    }else if(myEmail.value === '' || myEmail.value == null ){
-        
-         myMessage.innerHTML="you must write your email";
-    
-    }else if(myPhone.value === '' || myPhone.value == null || myPhone.value.length < 11 || isNaN(myPhone.value) ){
-        
-         myMessage.innerHTML="you must write your Phone";
-    
-    }else if(myPassword.value === '' || myPassword.value == null ){
-        
-         myMessage.innerHTML="you must write your password";
-    
-    }else if(myInput.value != myPassword.value){
-        
-         myMessage.innerHTML="you must confirm your password";
-    
-    }else{
-       location.href ="log-in.html";
+    if (myName.value === '' || myName.value == null) {
+        myMessage.innerHTML = "you must write your name";
+    } else if (myEmail.value === '' || myEmail.value == null) {
+
+        myMessage.innerHTML = "you must write your email";
+
+    } else if (myPhone.value === '' || myPhone.value == null || myPhone.value.length < 11 || isNaN(myPhone.value)) {
+
+        myMessage.innerHTML = "you must write your Phone";
+
+    } else if (myPassword.value === '' || myPassword.value == null) {
+
+        myMessage.innerHTML = "you must write your password";
+
+    } else if (myInput.value != myPassword.value) {
+
+        myMessage.innerHTML = "you must confirm your password";
+
+    } else {
+        location.href = "log-in.html";
     }
-  
+
 };
 
 /* for make focus in the first element in form */
 
-window.onload = function(){
-    
+window.onload = function () {
+
     'use strict';
     myName.focus();
 };
 
 /* for focus on the Email */
 
-myEmail.onfocus = function(){
+myEmail.onfocus = function () {
     'use strict';
     myMessage.innerHTML = 'please write your Gmail';
 };
 
-myEmail.onblur = function(){
+myEmail.onblur = function () {
     'use strict';
     myMessage.innerHTML = '';
 };
@@ -82,8 +81,8 @@ myEmail.onblur = function(){
 // };
 
 
-     
-     
+
+
 
 
 
